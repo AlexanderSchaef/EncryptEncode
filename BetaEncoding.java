80a57b192277729437942e4dcff7046138c5aa4d
AlexanderSchaef <112415716+AlexanderSchaef@users.noreply.github.com>
2023-10-21 17:00:26 -0400

    Initial Commits
    Created main driver

---
+++ BetaEncoding.java
@@ -0,0 +1,20 @@
+/**
+Independent Project testing encoding and encryption.
+Start date: 10/21/23
+README.md in the github repo will be used for a checklist.
+@author 1
+@author 2
+*/
+
+public class BetaEncoding
+  {
+    public static void main(String [] args)
+    {
+      // initialize variables
+      byte b;
+      // constants
+
+
+      
+    }
+  }
