baf346af6673825b0b7db4ba2f6f6144d3cef828
AlexanderSchaef <112415716+AlexanderSchaef@users.noreply.github.com>
2023-10-21 18:11:41 -0400

    Formatted Driver to Encode class

--- BetaEncoding.java
+++ BetaEncoding.java
@@ -2,46 +2,45 @@
 Independent Project testing encoding and encryption.
 Start date: 10/21/23
 README.md in the github repo will be used for a checklist.
-@author Alex Schaefer
+@author Alexander Schaefer
 @author Ashton Putnam
 */
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class BetaEncoding
   {
     public static void main(String [] args)
     {
       // initialize variables
-      byte [] b;
       String message = "Lorem ipsum dolor sit amet.";  // test message
-      
+
+      Encode eMessage = new Encode(message); 
+         // should store message as XX, length of message as XX
+         // and the different unique characters
+         
+      Random rand = new Random();
+      int randInt = rand.nextInt(1000);   // 0-999
+
       // constants
+      final int key = randInt;
       
-      // How to encode
-
-      
-      //input a message
-        //Scanner scanner = new Scanner(System.in)
-        // or
-        // Scanner scanner = new Scanner(new File("filename.txt"));
-      
-      // set byte array list to length of message
-
-
-      // encode to bytes
-
 
       // test byte back to string
-
+      // toString @Override method in Encode
+      
 
       // start encryption
 
-
+      // test toString values of encrypted (curiosity)
+      
       // test decryption
+      
+      // test decrypted byte arraylist back to string message
 
 
-      // test decrypted message against base message
+      // compare decrypted message against base message
 
       
     }
