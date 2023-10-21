7446f97cb7eedf4d90ffdc796191b467dee51910
AlexanderSchaef <112415716+AlexanderSchaef@users.noreply.github.com>
2023-10-21 17:23:52 -0400

    Starter layout
    Comment blocks for code, and variable initialization

--- BetaEncoding.java
+++ BetaEncoding.java
@@ -6,14 +6,42 @@ README.md in the github repo will be used for a checklist.
 @author 2
 */
 
+import java.util.Scanner;
+
 public class BetaEncoding
   {
     public static void main(String [] args)
     {
       // initialize variables
-      byte b;
+      byte [] b;
+      String message = "Lorem ipsum dolor sit amet.";  // test message
+      
       // constants
+      
+      // How to encode
 
+      
+      //input a message
+        //Scanner scanner = new Scanner(System.in)
+        // or
+        // Scanner scanner = new Scanner(new File("filename.txt"));
+      
+      // set byte array list to length of message
+
+
+      // encode to bytes
+
+
+      // test byte back to string
+
+
+      // start encryption
+
+
+      // test decryption
+
+
+      // test decrypted message against base message
 
       
     }
